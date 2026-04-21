2de467c690102c033f65530bb0bb0c47a9c768f3
GORDIAN12 <davidgordian667@gmail.com>
2026-04-21 14:41:25 -0600

    Update README and remove secrets

---
+++ app/services/github_service.py
@@ -0,0 +1,14 @@
+import requests
+from app.config import GITHUB_TOKEN, GITHUB_API_URL
+
+headers = {
+    "Authorization": f"Bearer {GITHUB_TOKEN}",
+    "Accept": "application/vnd.github+json"
+}
+
+def get_pull_request(owner: str, repo: str, pull_number: int):
+    url=f"{GITHUB_API_URL}/repos/{owner}/{repo}/pulls/{pull_number}"
+    response=requests.get(url, headers=headers)
+    response.raise_for_status()
+    return response.json()
+
